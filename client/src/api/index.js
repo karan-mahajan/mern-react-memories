97e8b7b1d90b1ecc2053f8d259200708d4ad15e9
mahajan69 <karanmahajan321@gmail.com>
2021-08-22 23:57:30 +0530

    Server Change

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080/posts';
+// const url = 'http://localhost:8080/posts';
 
-// const url = 'https://mern-project-one.herokuapp.com/posts';
+const url = 'https://mern-project-one.herokuapp.com/posts';
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
